14547ce3f2636bdcc5c0e9304086829e8c0745a1
Ryan Petrich <rpetrich@gmail.com>
2010-08-28 05:07:59 -0600

    Allow selecting shadow type

--- WBIconShadowFix.m
+++ WBIconShadowFix.m
@@ -4,6 +4,7 @@
 #include <dlfcn.h>
 
 static BOOL enabled;
+static NSInteger forcedShadowMode;
 
 #define kSettingsChangeNotification "ch.rpetri.wbiconshadowfix.settingschange"
 #define kSettingsFilePath "/var/mobile/Library/Preferences/ch.rpetri.wbiconshadowfix.plist"
@@ -25,14 +26,23 @@ CHOptimizedMethod(1, self, void, SBIconLabel, drawRect, CGRect, rect)
 		CHSuper(1, SBIconLabel, drawRect, rect);
 }
 
+CHOptimizedMethod(1, self, void, SBIconLabel, setInDock, BOOL, inDock)
+{
+	if (forcedShadowMode)
+		inDock = forcedShadowMode == 2;
+	CHSuper(1, SBIconLabel, setInDock, inDock);
+}
+
 CHConstructor {
 	CHLoadLateClass(SBIconLabel);
 	properDrawRect = (void *)class_getMethodImplementation(CHClass(SBIconLabel), @selector(drawRect:));
 	dlopen("/Library/MobileSubstrate/DynamicLibraries/WinterBoard.dylib", RTLD_LAZY);
 	CHHook(1, SBIconLabel, drawRect);
+	CHHook(1, SBIconLabel, setInDock);
 	CHAutoreleasePoolForScope();
 	NSDictionary *dict = [[NSDictionary alloc] initWithContentsOfFile:@kSettingsFilePath];
 	enabled = [[dict objectForKey:@"enabled"] ?: (id)kCFBooleanTrue boolValue];
+	forcedShadowMode = [[dict objectForKey:@"forcedShadowMode"] integerValue];
 	[dict release];
 	CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(), NULL, (void *)ReloadPreferences, CFSTR(kSettingsChangeNotification), NULL, CFNotificationSuspensionBehaviorHold);
 }
